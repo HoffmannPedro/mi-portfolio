4bc34c4234ecdd779865c95e2c064679c1647f78
Pedro Hoffmann <hoffmannpedro18@gmail.com>
2024-08-02 20:17:36 -0300

    funcionalidad del formulario

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,72 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
+import swal from 'sweetalert';
 
 export default function Contact() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
+  const [state, handleSubmit] = useForm("mqazqglq");
 
-  function encode(data) {
-    return Object.keys(data)
-      .map(
-        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-      )
-      .join("&");
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", name, email, message }),
-    })
-      .then(() => alert("Message sent!"))
-      .catch((error) => alert(error));
+  if (state.succeeded) {
+    setTimeout(() => {
+      window.location.reload(); // Recargar la página
+    }, 3000);
+    return swal("¡Mensaje enviado con éxito!", "Sera respondido a la brevedad", "success");
   }
 
   return (
-    <section
-      id="contact"
-      className="relative"
-    >
+    <section id="contact" className="relative">
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
-        {/* <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-          <iframe
-            width="100%"
-            height="100%"
-            title="map"
-            className="absolute inset-0"
-            style={{ filter: "opacity(0.7)" }}
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3286.555623647184!2d-58.77989091070315!3d-34.539484884422066!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bc9784d097b435%3A0x4f22f7d0db83d20f!2sCDF%2C%20Vicente%20Fatone%204536%2C%20B1665%20Jos%C3%A9%20C.%20Paz%2C%20Provincia%20de%20Buenos%20Aires%2C%20Argentina!5e0!3m2!1ses-419!2sus!4v1709771006833!5m2!1ses-419!2sus"
-          />
-          <div className="bg-gray-900 relative flex flex-wrap py-6 pr-5 rounded shadow-md">
-            <div className="lg:w-1/2 px-6">
-              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-                DIRECCIÓN
-              </h2>
-              <p className="mt-1">
-                Vicente Fatone 4536. <br />
-                Buenos Aires, Argentina <br />
-                JCP 1665
-              </p>
-            </div>
-            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
-              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-                CORREO ELECTRÓNICO
-              </h2>
-              <a className="text-indigo-400 leading-relaxed">
-                hoffmannpedro18@gmail.com
-              </a>
-              <h2 href="mailto:hoffmannpedro18@gmail.com" className="title-font font-semibold text-white tracking-widest text-xs mt-4">
-                NUMERO DE TELÉFONO
-              </h2>
-              <p className="leading-relaxed">11-2186-6976</p>
-            </div>
-          </div>
-        </div> */}
         <form
-          netlify
-          name="contact"
           onSubmit={handleSubmit}
           className="flex flex-col lg:w-2/4 lg:m-auto md:w-3/4 md:m-auto w-full md:py-8 md:mt-0"
         >
@@ -80,10 +29,7 @@ export default function Contact() {
             ¡Espero tu mensaje para empezar a hacer algo genial juntos!
           </p>
           <div className="relative mb-4">
-            <label
-              htmlFor="name"
-              className="leading-7 text-sm text-gray-400"
-            >
+            <label htmlFor="name" className="leading-7 text-sm text-gray-400">
               Nombre
             </label>
             <input
@@ -91,14 +37,15 @@ export default function Contact() {
               id="name"
               name="name"
               className="w-full bg-gray-800 rounded border-gray-700 focus:border-indigo-500 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setName(e.target.value)}
+            />
+            <ValidationError
+              prefix="Nombre"
+              field="name"
+              errors={state.errors}
             />
           </div>
           <div className="relative mb-4">
-            <label
-              htmlFor="email"
-              className="leading-7 text-sm text-gray-400"
-            >
+            <label htmlFor="email" className="leading-7 text-sm text-gray-400">
               Correo Electrónico
             </label>
             <input
@@ -106,26 +53,32 @@ export default function Contact() {
               id="email"
               name="email"
               className="w-full bg-gray-800 rounded border-gray-700 focus:border-indigo-500 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setEmail(e.target.value)}
+            />
+            <ValidationError
+              prefix="Correo Electrónico"
+              field="email"
+              errors={state.errors}
             />
           </div>
           <div className="relative mb-4">
-            <label
-              htmlFor="message"
-              className="leading-7 text-sm text-gray-400"
-            >
+            <label htmlFor="message" className="leading-7 text-sm text-gray-400">
               Mensaje
             </label>
             <textarea
               id="message"
               name="message"
               className="w-full bg-gray-800 rounded border-gray-700 focus:border-indigo-500 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setMessage(e.target.value)}
+            />
+            <ValidationError
+              prefix="Mensaje"
+              field="message"
+              errors={state.errors}
             />
           </div>
           <button
             type="submit"
             className="text-white bg-indigo-600 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+            disabled={state.submitting}
           >
             Enviar
           </button>
