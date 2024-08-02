c0eeec45415227d0a4f6d85f1aa2ebfe8c75d6cd
Pedro Hoffmann <hoffmannpedro18@gmail.com>
2024-08-02 17:23:23 -0300

    correcciones

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <section id="about">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+        <div className="lg:flex-grow flex flex-col lg:ml-10 lg:pr-24 md:w-1/2 md:pr-16 md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Hola, soy Pedro.
             <br className="lg:inline-block" />
@@ -32,7 +32,7 @@ export default function About() {
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
           <img
-            className="object-cover object-center rounded px-11"
+            className="object-cover object-center rounded m-auto lg:ml-auto"
             alt="img_profile"
             src="https://github.com/HoffmannPedro/mi-portfolio/blob/master/src/assets/pepe.png?raw=true"
           />
